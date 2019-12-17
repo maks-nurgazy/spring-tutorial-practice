bf9cbc7169a67f5d33b38642f56dab6b67daa18a
maks-nurgazy <maksatbek.bolushov@gmail.com>
2019-12-17 19:48:49 +0600

    ONE TO ONE

--- hibernate-tutorial/src/main/java/com/love/hibernate/demo/CreateStudentDemo.java
+++ hibernate-tutorial/src/main/java/com/love/hibernate/demo/CreateStudentDemo.java
@@ -1,5 +1,7 @@
 package com.love.hibernate.demo;
 
+import com.love.hibernate.entity.Instructor;
+import com.love.hibernate.entity.InstructorDetail;
 import com.love.hibernate.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,19 +11,26 @@ public class CreateStudentDemo {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                                 .configure("hibernate.cfg.xml")
-                                .addAnnotatedClass(Student.class)
+                                .addAnnotatedClass(Instructor.class)
+                                .addAnnotatedClass(InstructorDetail.class)
                                 .buildSessionFactory();
 
         Session session = factory.getCurrentSession();
 
         try {
-            Student maks = new Student("Koldoshbek","Tashiev","suiunalievich@gmail.com");
+
+            Instructor instructor = new Instructor("Maksatbek","Bolushov","maksnurgazy@gmail.com");
+            InstructorDetail detail = new InstructorDetail("youtube.com/bekanur","CODING");
+
+            instructor.setInstructorDetail(detail);
+
             session.beginTransaction();
-            session.save(maks);
-
-
+            System.out.println("Saving instructor..."+instructor);
+            session.save(instructor);
 
             session.getTransaction().commit();
+
+            System.out.println("Done very well!!!");
 
         }finally {
             session.close();
